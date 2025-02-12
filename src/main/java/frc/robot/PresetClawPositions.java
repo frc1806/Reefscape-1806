22d172a8b6444ce6706cc71fbb9e390de0ab433c
Dillon Woollums <dwooll94@gmail.com>
2025-02-12 16:30:08 -0600

    Preset claw positions

---
+++ src/main/java/frc/robot/PresetClawPositions.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public enum PresetClawPositions {
+    kHome(6.75, 0.0, 0.0),
+    kAlgaeNetForward(75.0, 0.0, 135.0),
+    kAlgaeNetBackward(75.0, 0.0, 45.0),
+    kAlgaeL3(43.7, 0.0, 200.0),
+    kAlgaeL2(35.8, 0.0, 200.0),
+    kCoralL4(75.0, 120.0, 0.0),
+    kCoralL3(50.0, 150.0, 0.0),
+    kCoralL2(34.0, 150.0, 0.0),
+    kCoralL1(25, 180.0, 0.0);
+    
+
+    private double mElevatorHeight;
+    private double mCoralClawAngle;
+    private double mAlgaeClawAngle;
+
+    /**
+     * Make a preset combination of elevator height, coral claw angle, and algae claw angle.
+     * @param elevatorHeight elevator height to the bottom of the carriage in inches
+     * @param coralClawAngle coral claw angle in degrees
+     * @param algaeClawAngle algae claw angle in degrees
+     */
+    private PresetClawPositions(double elevatorHeight, double coralClawAngle, double algaeClawAngle){
+        mElevatorHeight = elevatorHeight;
+        mCoralClawAngle = coralClawAngle;
+        mAlgaeClawAngle = algaeClawAngle;
+    }
+
+    /**
+     * Get elevator carriage bottom height in inches from the floor.
+     * @return
+     */
+    public double getElevatorHeight(){
+        return mElevatorHeight;
+    }
+
+    /**
+     * Get coral claw angle in degrees 
+     * @return
+     */
+    public double getCoralClawAngle(){
+        return mCoralClawAngle;
+    }
+
+    /**
+     * Get algae claw angle in degrees
+     * @return
+     */
+    public double getAlgaeClawAngle(){
+        return mAlgaeClawAngle;
+    }
+
+}
